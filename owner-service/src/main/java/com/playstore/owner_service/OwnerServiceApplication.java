ea372eba510a5874741ac1f180f4dc8de0d52bed
prashiklikhar009 <prashiklikhar009@gmail.com>
2025-09-09 20:51:14 +0000

    ok

--- owner-service/src/main/java/com/playstore/owner_service/OwnerServiceApplication.java
+++ owner-service/src/main/java/com/playstore/owner_service/OwnerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.playstore.owner_service;
 
+import com.playstore.owner_service.config.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@Import(WebSecurityConfig.class)
 public class OwnerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OwnerServiceApplication.class, args);

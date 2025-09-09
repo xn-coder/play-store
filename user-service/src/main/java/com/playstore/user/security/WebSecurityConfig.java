ee6290c98f5e22370923d8f104652af9847dd903
prashiklikhar009 <prashiklikhar009@gmail.com>
2025-09-09 20:43:52 +0000

    ok

--- user-service/src/main/java/com/playstore/user/security/WebSecurityConfig.java
+++ user-service/src/main/java/com/playstore/user/security/WebSecurityConfig.java
@@ -64,7 +64,9 @@ public class WebSecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/users/**").permitAll()
+                .requestMatchers("/api/users/auth/**").permitAll()
+                .requestMatchers("/api/users/health").permitAll()
+                .requestMatchers("/api/users/reviews/app/**").permitAll()
                 .requestMatchers("/h2-console/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                  .anyRequest().authenticated()

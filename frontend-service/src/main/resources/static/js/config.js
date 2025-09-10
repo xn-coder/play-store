db7e188deda8334502366d2317095e89129a49ed
prashiklikhar009 <prashiklikhar009@gmail.com>
2025-09-10 17:48:19 +0000

    otk

--- frontend-service/src/main/resources/static/js/config.js
+++ frontend-service/src/main/resources/static/js/config.js
@@ -1,6 +1,6 @@
-const API_GATEWAY_URL = '';
+const API_GATEWAY_URL = 'http://localhost:8080';
 
-const USER_SERVICE_URL = `${API_GATEWAY_URL}/user-service`;
-const OWNER_SERVICE_URL = `${API_GATEWAY_URL}/owner-service`;
-const NOTIFICATION_SERVICE_URL = `${API_GATEWAY_URL}/notification-service`;
-const SEARCH_SERVICE_URL = `${API_GATEWAY_URL}/search-service`;
+const USER_SERVICE_URL = `${API_GATEWAY_URL}/api/users`;
+const OWNER_SERVICE_URL = `${API_GATEWAY_URL}/api/owners`;
+const NOTIFICATION_SERVICE_URL = `${API_GATEWAY_URL}/notification`;
+const SEARCH_SERVICE_URL = `${API_GATEWAY_URL}/search`;

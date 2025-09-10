49c311ac6a4a8fc2aacb5dfef02bfda94d8688f1
xn-coder <prashiklikhar009@gmail.com>
2025-09-10 18:49:52 +0530

    commited

--- user-service/src/main/java/com/playstore/user/UserServiceApplication.java
+++ user-service/src/main/java/com/playstore/user/UserServiceApplication.java
@@ -5,3 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}

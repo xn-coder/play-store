4a811c7104959f0ca5a1863737bb371731a53a7c
prashiklikhar009 <prashiklikhar009@gmail.com>
2025-09-10 21:14:18 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,44 +1,36 @@
-{ pkgs, ... }: {
-  # channel = "stable-23.11"; # You can change the channel to use a different version of packages.
-
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{pkgs}: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.maven
     pkgs.jdk17
   ];
-
   # Sets environment variables in the workspace
   env = {};
+  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
+  # a firebase.json file is written into the user's directory
 
-  # Search for nix options here: https://devenv.sh/reference/options/
-  previews = [
-    {
-      port = 8081;
-      name = "API Gateway";
-    }
-    {
-      port = 8761;
-      name = "Eureka Server";
-    }
-    {
-      port = 8080;
-      name = "Frontend Service";
-    }
-    {
-      port = 8082;
-      name = "User Service";
-    }
-    {
-      port = 8083;
-      name = "Owner Service";
-    }
-    {
-      port = 8084;
-      name = "Notification Service";
-    }
-    {
-      port = 8085;
-      name = "Search Service";
-    }
-  ];
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+    workspace = {
+      onCreate = {
+      };
+    };
+    # Enable previews and customize configuration
+    previews = {
+      # enable = true;
+      # previews = {
+      #   web = {
+      #     command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+      #     manager = "web";
+      #   };
+      # };
+    };
+  };
 }

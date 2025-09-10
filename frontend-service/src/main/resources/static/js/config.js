b1de10ea288bd4a9ad9d099a061dc6c9b6d7f865
prashiklikhar009 <prashiklikhar009@gmail.com>
2025-09-10 09:33:44 +0000

    ohh ok

--- frontend-service/src/main/resources/static/js/config.js
+++ frontend-service/src/main/resources/static/js/config.js
@@ -1,4 +1,4 @@
-const API_GATEWAY_URL = 'http://localhost:8080';
+const API_GATEWAY_URL = '';
 
 const USER_SERVICE_URL = `${API_GATEWAY_URL}/user-service`;
 const OWNER_SERVICE_URL = `${API_GATEWAY_URL}/owner-service`;
